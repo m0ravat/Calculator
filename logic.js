344d689917687d9d295c2ce54cf486ce859c5a46
Muhammad Ravat <m0ravat736@gmail.com>
2024-06-19 18:56:43 +0100

    completion

--- logic.js
+++ logic.js
@@ -112,3 +112,105 @@ function reset(){
     c = 0;
     document.getElementById("statement").innerHTML=output;
 }
+
+
+
+var count = 0;
+var sum = 0;
+var average = 0;
+const marks = [];
+
+
+
+function input(){
+    if(document.getElementById("numm").value>100 || document.getElementById("numm").value<0){
+        alert("Invalid value");
+    }
+    else if(count >= 8){ 
+        alert("This calculator can only find the average for one years worth of 8 modules.")
+    }
+    else{
+        count++;
+        sum = sum + parseFloat(document.getElementById("numm").value);
+        marks.push(document.getElementById("numm").value);
+        document.getElementById("markss").innerHTML=marks.toString();
+        average = sum/count;
+        document.getElementById("resultingNum").innerHTML=average;
+        if (average < 40){
+            document.getElementById("resultingDegree").innerHTML="Fail";
+        }
+        else if (average>=40 && average<50){
+            document.getElementById("resultingDegree").innerHTML="Third Class honours";
+        }
+        else if(average>=50 && average<60){
+            document.getElementById("resultingDegree").innerHTML="Lower second class honours 2:2";
+        }
+        else if(average>=60 && average<70){
+            document.getElementById("resultingDegree").innerHTML="Upper second class honours 2:1";
+        }
+        else{
+            document.getElementById("resultingDegree").innerHTML="First class honours";
+        }
+        document.getElementById("numm").value="";
+    }
+}
+
+function resetDeg(){
+    count=0;
+    sum=0;
+    average=0;
+    degree="";
+    for(var x =0; x < 7; x++){
+        marks.pop();
+    }
+    document.getElementById("resultingDegree").innerHTML="";
+    document.getElementById("resultingNum").innerHTML="";
+    document.getElementById("markss").innerHTML="";
+    document.getElementById("numm").value="";
+}
+
+
+var final = 0;
+var y1 = 0;
+var y2 = 0;
+var finalStatement = "";
+function calc(){
+    if(parseFloat(document.getElementById("y1").value) <0 || parseFloat(document.getElementById("y2").value) <0){
+        alert("Cant put negative values");
+    }
+    else if(parseFloat(document.getElementById("y1").value) + parseFloat(document.getElementById("y2").value) != 100){
+        alert("Weighting doesn't add up to 100/incomplete");
+    }
+    else if(parseFloat(document.getElementById("Y1").value) <0 || parseFloat(document.getElementById("Y2").value) <0){
+        alert("Average mark can't be negative");
+    }
+    else if(parseFloat(document.getElementById("Y1").value) >100 || parseFloat(document.getElementById("Y2").value) > 100){
+        alert("Average mark can't be above 100");
+    }
+    else if (document.getElementById("Y1").value=="" || document.getElementById("Y2").value==""){
+        alert("Average marks incomplete");
+    }
+    else{
+        y1 = parseFloat(document.getElementById("y1").value) * parseFloat(document.getElementById("Y1").value);
+        y2 = parseFloat(document.getElementById("y2").value) * parseFloat(document.getElementById("Y2").value);
+        final = parseFloat(y1 + y2);
+        final = parseFloat(final / 100);
+        finalStatement=final;
+        if (final < 40){
+            finalStatement = finalStatement + "% -  Fail ";
+        }
+        else if (final<50 && final>=40){
+            finalStatement = finalStatement + "% -  Third class Honours ";
+        }
+        else if (final<60 && final>=50){
+            finalStatement = finalStatement + "% -  Lower Second Class Honours 2:2 ";
+        }
+        else if (final<70 && final>=60){
+            finalStatement = finalStatement + "% -  Upper Second Class Honours 2:1 ";
+        }
+        else{
+            finalStatement = finalStatement + "% -  First Class Honours";
+        }
+        document.getElementById("degree").innerHTML = finalStatement;
+    }
+}
